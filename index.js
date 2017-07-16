b3be45317415b6f983e0e7694fb2c34b5deb5dc0
cpcp <geekarist@users.noreply.github.com>
2017-07-16 10:26:00 +0200

    Display all fields

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ var facebook = require('./secret/facebook.json');
 
 var token=`${facebook.appId}|${facebook.secretKey}`;
 
-request(
-    `https://graph.facebook.com/v2.8/antrebloc94?fields=name&access_token=${token}`,
+var fields = [ 'name', 'hours', 'location', 'website', 'emails', 'cover',
+'link', 'parking', 'phone', 'public_transit', 'single_line_address'];
+
+var joinedFields = fields.join(',');
+
+var baseUrl = 'https://graph.facebook.com/v2.8';
+var url = `${baseUrl}/antrebloc94?fields=${joinedFields}&access_token=${token}`;
+
+// function mapPlace(body) {
+//     var originalPlace = JSON.parse(body);
+//     var mappedPlace = {
+//         id: originalPlace.id,
+//         name: originalPlace.name
+//     };
+//     return mappedPlace;
+// }
+
+request(url,
     (error, response, html) => {
         console.log(`Response: ${response.statusCode}`);
+        // var mappedResponse = mapPlace(response.body);
         console.log(response.body);
     });
