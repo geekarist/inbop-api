9239ce5436c5647d124cb31f052ccec889efa924
cpcp <geekarist@users.noreply.github.com>
2017-07-16 10:54:35 +0200

    Map FB fields

--- index.js
+++ index.js
@@ -3,26 +3,52 @@ var facebook = require('./secret/facebook.json');
 
 var token=`${facebook.appId}|${facebook.secretKey}`;
 
-var fields = [ 'name', 'hours', 'location', 'website', 'emails', 'cover',
-'link', 'parking', 'phone', 'public_transit', 'single_line_address'];
+var fields = [ 'name', 'username', 'hours', 'location', 'website', 'emails', 'cover', 'link', 'parking', 'phone', 'public_transit', 'single_line_address', 'about', 'price_range'];
 
 var joinedFields = fields.join(',');
 
 var baseUrl = 'https://graph.facebook.com/v2.8';
 var url = `${baseUrl}/antrebloc94?fields=${joinedFields}&access_token=${token}`;
 
-// function mapPlace(body) {
-//     var originalPlace = JSON.parse(body);
-//     var mappedPlace = {
-//         id: originalPlace.id,
-//         name: originalPlace.name
-//     };
-//     return mappedPlace;
-// }
+function mapPlace(body) {
+    var originalPlace = JSON.parse(body);
+    var mappedPlace = {
+        id: originalPlace.id,
+        name: originalPlace.name,
+        city: originalPlace.location.city,
+        'img-url': originalPlace.cover.source,
+        url: originalPlace.website,
+        "facebook": originalPlace.username,
+        "email": originalPlace.emails.join(', '),
+        "description": originalPlace.about,
+        "position": {
+          "lat": originalPlace.location.latitude,
+          "lon": originalPlace.location.longitude,
+          "address": originalPlace.location.street,
+          "transport": originalPlace.public_transit,
+        },
+        "hours": {
+          "weekdays": {
+            "opening": originalPlace.hours.mon_1_open,
+            "closing": originalPlace.hours.mon_1_close
+          },
+          "weekend": {
+            "opening": originalPlace.hours.sat_1_open,
+            "closing": originalPlace.hours.sat_1_close
+          }
+        },
+        "price": {
+          "adult": originalPlace.price_range,
+          "student": originalPlace.price_range,
+          "child": originalPlace.price_range
+        }
+    };
+    return mappedPlace;
+}
 
 request(url,
     (error, response, html) => {
         console.log(`Response: ${response.statusCode}`);
-        // var mappedResponse = mapPlace(response.body);
-        console.log(response.body);
+        var mappedResponse = mapPlace(response.body);
+        console.log(JSON.stringify(mappedResponse, null, 2));
     });
